e8bf73fd56518a44259f3c11d782e44325e36dc5
CyberVitexus <info@vitexsoftware.cz>
2026-03-20 13:30:40 +0100

    feat: Add Ease autoloader and correct the application autoloader's prefix length.

--- debian/autoload.php
+++ debian/autoload.php
@@ -1,10 +1,13 @@
 <?php
 // Debian autoloader for abraflexi-pricefixer
+require_once '/usr/share/php/Ease/autoload.php';
 require_once '/usr/share/php/AbraFlexi/autoload.php';
 // PSR-4 autoloader for application classes
 spl_autoload_register(function (string $class): void {
-    if (strncmp('AbraFlexi\\PriceFix\\', $class, 20) === 0) {
-        $file = '/usr/lib/abraflexi-pricefixer/PriceFix/' . str_replace('\\', '/', substr($class, 20)) . '.php';
-        if (file_exists($file)) { require $file; }
+    if (strncmp('AbraFlexi\\PriceFix\\', $class, 19) === 0) {
+        $file = '/usr/lib/abraflexi-pricefixer/PriceFix/' . str_replace('\\', '/', substr($class, 19)) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 });
